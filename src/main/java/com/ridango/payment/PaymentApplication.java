da5be53a4f5fdcbbd87d8b7ae01eed059ea6f75b
UkuSoome <Soome.Uku@gmail.com>
2024-09-19 17:40:13 +0300

    Initial controller working

--- src/main/java/com/ridango/payment/PaymentApplication.java
+++ src/main/java/com/ridango/payment/PaymentApplication.java
@@ -1,10 +1,10 @@
-package com.rindago.payment;
+package com.ridango.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ridango.payment"})
 public class PaymentApplication {
 
 	public static void main(String[] args) {
